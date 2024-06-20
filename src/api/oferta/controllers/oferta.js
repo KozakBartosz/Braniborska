0d45ee483f70fbf7be2c812a295c50a0ec114eb3
KozakBartosz <bartoszkozak94@gmail.com>
2024-06-21 01:07:34 +0200

    First init

---
+++ src/api/oferta/controllers/oferta.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * oferta controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::oferta.oferta');
---
+++ src/api/oferta/services/oferta.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * oferta service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::oferta.oferta');
